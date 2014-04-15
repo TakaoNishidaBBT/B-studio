9b00f974422a6ddca9624a94c8b84917e8793bf1
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2014-04-15 18:06:06 +0900

    new

---
+++ bs-admin/user/users.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * B-studio : Contents Management System
+ * Copyright (c) BigBeat Inc. all rights reserved. (http://www.bigbeat.co.jp)
+ *
+ * Licensed under the GPL, LGPL and MPL Open Source licenses.
+*/
+	$g_auth_users = array(
+		array(
+			'user_name'	=> 'サイト管理者',
+			'user_id'	=> 'admin',
+			'pwd'		=> '21232f297a57a5a743894a0e4a801fc3',
+			'user_auth'	=> 'admin',
+		)
+	);
