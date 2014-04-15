d3bf82712323940c400fb71b8b3e65f417a239ca
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2014-04-15 18:06:06 +0900

    new

---
+++ bs-admin/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * B-studio : Contents Management System
+ * Copyright (c) BigBeat Inc. all rights reserved. (http://www.bigbeat.co.jp)
+ *
+ * Licensed under the GPL, LGPL and MPL Open Source licenses.
+*/
+	require_once('./controller/controller.php');
