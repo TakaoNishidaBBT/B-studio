534483ef84fd78dde0810902925bf8c7fb321e62
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2014-04-22 13:08:20 +0900

    fixed

--- bs-admin/js/bframe_modal_window.js
+++ bs-admin/js/bframe_modal_window.js
@@ -171,7 +171,7 @@
 			overlay.style.display = 'none';
 			modal_window.style.display = 'none';
 			modal_window.style.opacity = 0;
-			containerBody.src = '';
+			containerBody.src = 'about:blank';
 			window_status = false;
 
 			executeCallBack(param);
@@ -239,7 +239,8 @@
 			drag_overlay.style.position = 'absolute';
 			drag_overlay.style.top = 0;
 			drag_overlay.style.left = 0;
-			drag_overlay.style.opacity = 0;
+			drag_overlay.style.opacity = 0.5;
+			drag_overlay.style.backgroundColor = '#f00';
 
 			document.body.appendChild(drag_overlay);
 
