58e4be1655742e8de5a83e9c6862103244dee41c
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2014-06-04 15:40:48 +0900

    fixed

--- bs-admin/module/filemanager/view/view_index.php
+++ bs-admin/module/filemanager/view/view_index.php
@@ -9,12 +9,12 @@
 	<div id="splitter" name="splitter" class="splitter bframe_splitter" param="margin:12"></div>
 	<div id="filemanager_pain" class="bframe_splitter_pain bframe_adjustwindow" param="margin:12">
 		<ul class="upload_control">
+			<li class="select"><span id="bframe_pain_disp_change">表示 ： </span></li>
 			<li>
 				<div id="upload_button" class="input_container">
-					<a href="index.php?terminal_id=<?php echo TERMINAL_ID ?>&module=filemanager&page=upload&method=init&session=<?php echo $this->module ?>" title="アップロード" class="upload-button bframe_upload" onclick="activateModalWindow(this, 440, 500, reloadTree); return false;">アップロード</a>
+					<a href="index.php?terminal_id=<?php echo TERMINAL_ID ?>&module=filemanager&page=upload&method=init&session=<?php echo $this->module ?>" title="アップロード" class="upload-button bframe_upload" onclick="activateModalWindow(this, 440, 500, reloadTree); return false;"><img src="images/common/save.png" alt="アップロード" />アップロード</a>
 				</div>
 			</li>
-			<li class="select"><span id="bframe_pain_disp_change">表示 ： </span></li>
 		</ul>
 		<div class="pain_container">
 			<div id="bframe_pain" class="bframe_pain bframe_adjustparent" param="margin:74"></div>
