ee1bb37ea88f0b4c6938c3cd96d275215a201932
TakaoNishida <takao@mvc.biglobe.ne.jp>
2014-06-11 07:27:48 +0900

    fixed

--- bs-admin/module/filemanager/view/view_index.php
+++ bs-admin/module/filemanager/view/view_index.php
@@ -1,13 +1,13 @@
 <body>
 	<h2><span class="accent"></span>ファイル管理</h2>
 	<div class="header"></div>
-	<div id="bframe_tree_container" class="bframe_tree_container bframe_splitter_pain bframe_adjustwindow" param="margin:12">
+	<div id="bframe_tree_container" class="bframe_tree_container bframe_splitter_pain bframe_adjustwindow" param="margin:10">
 		<div id="tree_box" class="bframe_adjustparent" param="margin:1">
 			<?php echo $this->tree->getHtml(); ?>
 		</div>
 	</div>
-	<div id="splitter" name="splitter" class="splitter bframe_splitter" param="margin:12"></div>
-	<div id="filemanager_pain" class="bframe_splitter_pain bframe_adjustwindow" param="margin:12">
+	<div id="splitter" name="splitter" class="splitter bframe_splitter" param="margin:10"></div>
+	<div id="filemanager_pain" class="bframe_splitter_pain bframe_adjustwindow" param="margin:10">
 		<ul class="upload_control">
 			<li class="select"><span id="bframe_pain_disp_change">表示 ： </span></li>
 			<li>
@@ -17,7 +17,7 @@
 			</li>
 		</ul>
 		<div class="pain_container">
-			<div id="bframe_pain" class="bframe_pain bframe_adjustparent" param="margin:74"></div>
+			<div id="bframe_pain" class="bframe_pain bframe_adjustparent" param="margin:62"></div>
 		</div>
 	</div>
 	<div class="footer"></div>
