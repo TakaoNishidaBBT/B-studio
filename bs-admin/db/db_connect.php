7e5867604ff13080d195da900b97bc52b0751b2a
TakaoNishida <takao@mvc.biglobe.ne.jp>
2014-06-27 07:37:20 +0900

    fixed

---
+++ bs-admin/db/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * B-studio : Contents Management System
+ * Copyright (c) BigBeat Inc. all rights reserved. (http://www.bigbeat.co.jp)
+ *
+ * Licensed under the GPL, LGPL and MPL Open Source licenses.
+*/
+	define("B_DB_NME", "bstudio");
+	define("B_DB_USR", "bstudio");
+	define("B_DB_PWD", "bstudio");
+	define("B_DB_SRV", "localhost");
+	define("B_DB_CHARSET", "utf8");
