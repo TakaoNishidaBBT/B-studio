492ed2500f1122df88d69e79cb9cef849f938938
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2014-10-15 19:14:33 +0900

    fixed

--- bs-admin/js/bframe_preview.js
+++ bs-admin/js/bframe_preview.js
@@ -5,55 +5,60 @@
  * Licensed under the GPL, LGPL and MPL Open Source licenses.
 */
 	// -------------------------------------------------------------------------
-	// class bframe.Preview
+	// class bframe.preview
 	// 
 	// -------------------------------------------------------------------------
-	bframe.Preview = function() {
-		var cb = new Array;
-
-		this.submit = function(fname, url, method, iframe_name) {
-			previewExecuteCallBack();
-
-			// set form object
-			if(fname) {
-				var form = document.forms[fname];
-			}
-			else {
-				var form = document.forms[0];
-			}
-
-			var save_action = form.action;
-			var save_target = form.target;
-
-			form.action = url;
-			form.target = iframe_name;
-
-			bframe.appendHiddenElement(form, 'method', method);
-
-			form.submit();
-
-			form.action = save_action;
-			form.target = save_target;
-		}
-
-		this.registCallBackFunction = function(func) {
-			cb.push(func);
-		}
-
-		this.removeCallBackFunction = function(func) {
-			for(var i=0 ; i<cb.length ; i++) {
-				if(func == cb[i]) {
-					cb.splice(i, 1);
+	bframe.preview = function() {
+		var cb = new Array,
+			previewExecuteCallBack = function() {
+				for(var i=0 ; i<cb.length ; i++) {
+					func = cb[i];
+					func();
 				}
 			}
-		}
 
-		previewExecuteCallBack = function() {
-			for(var i=0 ; i<cb.length ; i++) {
-				func = cb[i];
-				func();
-			}
-		}
-	}
+		return {
+			submit: function(fname, url, method, iframe_name) {
+				// execute callback function before submit
+				previewExecuteCallBack();
 
-	bframe.preview = new bframe.Preview;
+				// set form object
+				if(fname) {
+					var form = document.forms[fname];
+				}
+				else {
+					var form = document.forms[0];
+				}
+
+				// save original action and target
+				var save_action = form.action;
+				var save_target = form.target;
+
+				// change action and target from parameter
+				form.action = url;
+				form.target = iframe_name;
+
+				// append method from parameter
+				bframe.appendHiddenElement(form, 'method', method);
+
+				// submit
+				form.submit();
+
+				// restore original action and target
+				form.action = save_action;
+				form.target = save_target;
+			},
+
+			registCallBackFunction: function(func) {
+				cb.push(func);
+			},
+
+			removeCallBackFunction: function(func) {
+				for(var i=0 ; i<cb.length ; i++) {
+					if(func == cb[i]) {
+						cb.splice(i, 1);
+					}
+				}
+			},
+		}
+	}();
