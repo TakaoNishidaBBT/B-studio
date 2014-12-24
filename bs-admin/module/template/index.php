03462d7cb8398ae0f214790935216245248a73a2
TakaoNishidaMac <takao@mac-book-air.local>
2014-12-25 07:45:06 +0900

    fixed
    add loading effect

--- bs-admin/module/template/index.php
+++ bs-admin/module/template/index.php
@@ -16,6 +16,7 @@
 
 			$this->html_header->appendProperty('css','<link href="css/template.css" type="text/css" rel="stylesheet" media="all" />');
 			$this->html_header->appendProperty('script','<script src="js/bframe_splitter.js" type="text/javascript"></script>');
+			$this->html_header->appendProperty('script','<script src="js/bframe_effect.js" type="text/javascript"></script>');
 
 			// HTMLヘッダー出力
 			$this->showHtmlHeader();
