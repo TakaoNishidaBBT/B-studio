409b29769174f2b353415bb63238362a59132c3d
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2015-04-10 16:15:20 +0900

    fixed

--- bs-admin/module/resource/view/view_index.php
+++ bs-admin/module/resource/view/view_index.php
@@ -1,4 +1,4 @@
-<body onload="bframe.effect.fadeIn(document.body, 100, 0, 100, 50);" style="opacity:0">
+<body>
 	<h2><span class="accent"></span>リソース管理<span><?php echo $this->version_info; ?></span></h2>
 	<div class="header"></div>
 	<div id="bframe_tree_container" class="bframe_tree_container bframe_splitter_pain bframe_adjustwindow" param="margin:10">
