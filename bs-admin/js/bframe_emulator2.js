c3a1c7b34afb1f6b492b14f95420b7a9fe967101
TakaoNishidaBBT <nishida@bigbeat.co.jp>
2015-10-30 14:12:20 +0900

    fixed

--- bs-admin/js/bframe_emulator2.js
+++ bs-admin/js/bframe_emulator2.js
@@ -12,39 +12,17 @@
 	// -------------------------------------------------------------------------
 	function deviceEmulator() {
 		var meta = bframe.getPageInfo();
-		var width = bframe.getParam('width', meta['viewport'], ',', '=');
-		var scale = bframe.getParam('initial-scale', meta['viewport'], ',', '=');
-		var cw = document.body.clientWidth;
-		var ch = document.body.clientHeight;
+		if(meta['viewport']) {
+			var width = bframe.getParam('width', meta['viewport'], ',', '=');
+			var scale = bframe.getParam('initial-scale', meta['viewport'], ',', '=');
+		}
 
 		if(width && width.toLowerCase() != 'device_width') {
-			width = width.replace('px', '');
+			width = parseInt(width);
+		}
+		if(!width) {
+			width = 980;
 		}
 
 		parent.bframeEmulator.setViewport(width, scale);
-/*	
-		onResize();	
-console.log('width', width, 'initial_scale', initial_scale);
-
-//		document.body.style.transform = 'scale(0.5, 0.5)';
-
-		bframe.addEventListner(window, 'resize' , onResize);
-
-		function onResize() {
-			var w = window.innerWith || document.documentElement.clientWidth || document.body.clientWidth;
-			var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-
-console.log('w', w, 'h', h, 'cw', cw, 'ch', ch);
-
-			if(width == 'device-width') return;
-
-			width = width.replace('px', '');
-			ratio = w / width;
-			var transform = 'translate(-' + ((1-ratio) * cw)/2 + 'px, -' + ((1-ratio) * ch)/2 + 'px) scale(' + ratio + ') '; 
-console.log('transform', transform);
-//			document.body.style.transform = 'none';
-//			document.body.style.transform = transform;
-		}
-*/
 	}
