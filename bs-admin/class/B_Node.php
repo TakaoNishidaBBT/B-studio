a163e13b98616d85a7cde4b267431e3631623559
Takao Nishida <nishida@bigbeat.co.jp>
2016-06-24 15:32:30 +0900

    fixed

--- bs-admin/class/B_Node.php
+++ bs-admin/class/B_Node.php
@@ -709,7 +709,6 @@
 			}
 			else if($this->parent && $this->node_type != 'folder') {
 				$info = pathinfo($mypath);
-//				$data[$mypath] = $this->contents_id . '.' . strtolower($info['extension']);
 				$data[$mypath] = $this->contents_id . '.' . $info['extension'];
 				$data[$this->node_id] = $mypath;
 			}
@@ -727,7 +726,6 @@
 			else if($this->parent) {
 				$info = pathinfo($mypath);
 				if($this->node_type != 'folder') {
-//					$data[$mypath] = $this->contents_id . '.' . strtolower($info['extension']);
 					$data[$mypath] = $this->contents_id . '.' . $info['extension'];
 				}
 				else {
