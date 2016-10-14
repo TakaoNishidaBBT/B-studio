c06acef6b752a7948e1fe7bf6617a08058895838
Takao Nishida <nishida@bigbeat.co.jp>
2016-10-14 13:39:25 +0900

    fixed

---
+++ bs-admin/user/config/_users.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * B-studio : Contents Management System
+ * Copyright (c) BigBeat Inc. all rights reserved. (http://www.bigbeat.co.jp)
+ *
+ * Licensed under the GPL, LGPL and MPL Open Source licenses.
+*/
+	$g_auth_users = array(
+		array(
+			'user_name'	=> '%USER_NAME%',
+			'user_id'	=> '%USER_ID%',
+			'pwd'		=> '%PASSWORD%',
+			'language'	=> '%LANGUAGE%',
+			'user_auth'	=> 'super_admin',
+		)
+	);
