ef6ac7b7d88f55304262033464ed2a9cafa0e0e5
Takao Nishida <nishida@bigbeat.co.jp>
2016-10-21 20:25:26 +0900

    fixed

--- install/view/view_confirm.php
+++ install/view/view_confirm.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache">
-<meta http-equiv="Expires" content="Thu, 01 Dec 1994 16:00:00 GMT"> 
-<meta http-equiv="Content-Style-Type" content="text/css">
-<meta http-equiv="Content-Script-Type" content="text/javascript">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8" />
 <link rel="stylesheet" href="css/install.css" type="text/css" media="all" />
 <title>Install B-studio</title>
 </head>
--- install/view/view_error.php
+++ install/view/view_error.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache">
-<meta http-equiv="Expires" content="Thu, 01 Dec 1994 16:00:00 GMT"> 
-<meta http-equiv="Content-Style-Type" content="text/css">
-<meta http-equiv="Content-Script-Type" content="text/javascript">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8" />
 <link rel="stylesheet" href="css/install.css" type="text/css" media="all" />
-<title>B-studioのインストール</title>
+<title>Install B-studio</title>
 </head>
 <body>
-	<h1>B-studioのインストール</h1>
+	<h1>Install B-studio</h1>
 
 	<p class="error"><?php echo $this->error_message; ?></p>
 	<fieldset>
@@ -21,8 +15,7 @@
 	</fieldset>
 
 	<ul class="control">
-		<li><input type="button" class="button" name="button" value="戻る" onclick="history.back();"  /></li>
+		<li><input type="button" class="button" name="button" value="Back" onclick="history.back();"  /></li>
 	</ul>
-
 </body>
 </html>
