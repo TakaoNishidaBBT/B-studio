dfdd4d602bfd61e8c832e98e22fc8a9be25c847e
TakaoNishidaMac <takaoooo@icloud.com>
2017-03-08 07:28:39 +0900

    fixed

--- bs-admin/module/category/view/view_property.php
+++ bs-admin/module/category/view/view_property.php
@@ -1,7 +1,7 @@
 <body spellcheck="false">
 	<div class="bframe_adjustparent" id="contents_container">
-		<div id="contents" class="bframe_adjustparent" param="margin:2">
-			<div class="main_container bframe_adjustwindow" param="margin:0" >
+		<div id="contents" class="bframe_adjustparent" data-param="margin:2">
+			<div class="main_container bframe_adjustwindow" data-param="margin:0" >
 				<form name="F1" id="F1" method="post" action="index.php">
 					<?php
 						echo $this->tab_control->getHtml();
