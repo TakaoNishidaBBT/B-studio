600b6d2ccba8be9ea766c164189d5de95ecc6908
TakaoNishidaMac <takaoooo@icloud.com>
2017-03-20 07:40:32 +0900

    fixed

--- bs-admin/class/B_FileNode.php
+++ bs-admin/class/B_FileNode.php
@@ -206,10 +206,7 @@
 			if($this->node_id == $node_id) {
 				$list[$category] = true;
 			}
-			if($path && substr($path, -1) != '/') {
-				$path.= '/';
-			}
-			$list['path'] = $path . $list['node_name'];
+			$list['path'] = $this->path;
 			$list['disp_seq'] = $disp_seq;
 			$list['order'] = $this->order;
 			return $list;
