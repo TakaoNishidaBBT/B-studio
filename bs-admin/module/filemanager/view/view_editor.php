9a6e98f88c884bea1f33882be4cee0e7bcf8e2e7
TakaoNishidaMac <takaoooo@icloud.com>
2018-02-03 06:59:55 +0900

    fixed

--- bs-admin/module/filemanager/view/view_editor.php
+++ bs-admin/module/filemanager/view/view_editor.php
@@ -1,12 +1,14 @@
 <body spellcheck="false">
-	<div class="bframe_adjustparent" id="contents_container">
-		<div class="main_container bframe_adjustwindow" data-param="margin:0" >
-			<form name="F1" id="F1" method="post" action="index.php">
-				<?php
-					echo $this->tab_control->getHtml();
-					echo $this->editor->getHtml();
-				?>
-			</form>
+	<div class="bframe_adjustparent" id="contents_editor_container">
+		<div id="contents_editor" class="bframe_adjustparent">
+			<div class="main_container bframe_adjustwindow" data-param="margin:0" >
+				<form name="F1" id="F1" method="post" action="index.php">
+					<?php
+						echo $this->tab_control->getHtml();
+						echo $this->editor->getHtml();
+					?>
+				</form>
+			</div>
 		</div>
 	</div>
 </body>
--- bs-admin/module/resource/view/view_editor.php
+++ bs-admin/module/resource/view/view_editor.php
@@ -1,6 +1,6 @@
 <body spellcheck="false">
-	<div class="bframe_adjustparent" id="contents_container">
-		<div id="contents" class="bframe_adjustparent">
+	<div class="bframe_adjustparent" id="contents_editor_container">
+		<div id="contents_editor" class="bframe_adjustparent">
 			<div class="main_container bframe_adjustwindow" data-param="margin:0" >
 				<form name="F1" id="F1" method="post" action="index.php">
 					<?php
