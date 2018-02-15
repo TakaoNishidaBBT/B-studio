92a94ee0f343f16cfb68e5638f98987a18e9ae95
Takao Nishida <nishida@bigbeat.co.jp>
2018-02-15 18:24:04 +0900

    fixed

--- bs-admin/module/version/view/view_list_confirm.php
+++ bs-admin/module/version/view/view_list_confirm.php
@@ -1,12 +1,14 @@
 <body>
 	<h2 class="version"><?php echo __('Versions'); ?><span><?php echo $this->version_info; ?></span></h2>
-	<div class="contents bframe_adjustwindow bframe_scroll" data-param="margin:0"">
-		<form name="F1" id="F1" method="post" action="index.php" target="main">
-			<div class="version-info">
-				<?php echo $this->version_information->getHtml(); ?>
-				<p><?php echo __('will be set.'); ?></p>
-			</div>
-			<?php echo $this->version_control_confirm->getHtml(); ?>
-		</form>
+	<div class="contents bframe_adjustwindow bframe_scroll" data-param="margin:0">
+		<div class="inner-contents">
+			<form name="F1" id="F1" method="post" action="index.php" target="main">
+				<div class="version-info">
+					<?php echo $this->version_information->getHtml(); ?>
+					<p><?php echo __('will be set.'); ?></p>
+				</div>
+				<?php echo $this->version_control_confirm->getHtml(); ?>
+			</form>
+		</div>
 	</div>
 </body>
--- bs-admin/module/version/view/view_list_result.php
+++ bs-admin/module/version/view/view_list_result.php
@@ -1,12 +1,14 @@
 <body>
 	<h2 class="version"><?php echo __('Versions'); ?><span><?php echo $this->version_info; ?></span></h2>
-	<div class="contents bframe_adjustparent bframe_scroll" data-param="margin:133"">
-		<form name="F1" id="F1" method="post" action="index.php" target="main">
-			<div class="version-info">
-				<?php echo $this->version_information->getHtml(); ?>
-				<p><?php echo $this->action_message; ?></p>
-			</div>
-			<?php echo $this->version_control_result->getHtml(); ?>
-		</form>
+	<div class="contents bframe_adjustwindow bframe_scroll" data-param="margin:0">
+		<div class="inner-contents">
+			<form name="F1" id="F1" method="post" action="index.php" target="main">
+				<div class="version-info">
+					<?php echo $this->version_information->getHtml(); ?>
+					<p><?php echo $this->action_message; ?></p>
+				</div>
+				<?php echo $this->version_control_result->getHtml(); ?>
+			</form>
+		</div>
 	</div>
 </body>
