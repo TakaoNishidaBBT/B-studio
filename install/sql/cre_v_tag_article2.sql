f44b77c8fcb646921f131478c6f9d7d3023b12a9
Takao Nishida <nishida@bigbeat.co.jp>
2020-09-14 09:16:26 +0900

    fixed

--- install/sql/cre_v_tag_article2.sql
+++ install/sql/cre_v_tag_article2.sql
@@ -1,5 +1,5 @@
 create or replace view %DB_PREFIX%v_tag_article2 as
-select max(tag_id) tag_id, b.node_name tag_name, b.disp_seq
+select max(tag_id) tag_id, b.node_name tag_name, b.slug slug, b.disp_seq
 from %DB_PREFIX%article_tag2 a
 left join %DB_PREFIX%tag2 b
 on a.tag_id = b.node_id
--- install/sql/cre_v_tag_article3.sql
+++ install/sql/cre_v_tag_article3.sql
@@ -1,5 +1,5 @@
 create or replace view %DB_PREFIX%v_tag_article3 as
-select max(tag_id) tag_id, b.node_name tag_name, b.disp_seq
+select max(tag_id) tag_id, b.node_name tag_name, b.slug slug, b.disp_seq
 from %DB_PREFIX%article_tag3 a
 left join %DB_PREFIX%tag3 b
 on a.tag_id = b.node_id
