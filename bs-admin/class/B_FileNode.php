b960138685c6f6499819b70501eaa37725044b02
BIGBEAT\nishida <nishida@bigbeat.co.jp>
2024-01-31 14:55:18 +0900

    fixed

--- bs-admin/class/B_FileNode.php
+++ bs-admin/class/B_FileNode.php
@@ -291,8 +291,10 @@
 					}
 				}
 				else {
-					if(isset($this->parent->path) && $this->parent->path) $this->path = __getPath($this->parent->path, $this->file_name);
-					$this->node_id = __getPath($this->parent->path, $this->file_name);
+					if(isset($this->parent->path) && $this->parent->path) {
+						$this->path = __getPath($this->parent->path, $this->file_name);
+						$this->node_id = __getPath($this->parent->path, $this->file_name);
+					}
 					$this->thumbnail_image_path = $this->getThumbnailImgPath($this->path);
 					$this->thumb = B_UPLOAD_THUMBDIR . str_replace('/', '-', $this->thumbnail_image_path);
 					if(file_exists($thumb)) {
