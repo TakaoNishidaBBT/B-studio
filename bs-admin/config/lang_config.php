f4f741d8f75b3dc096531b774e10be20cca468a6
BIGBEAT\nishida <nishida@bigbeat.co.jp>
2024-02-01 16:34:26 +0900

    fixed

--- bs-admin/config/lang_config.php
+++ bs-admin/config/lang_config.php
@@ -6,4 +6,6 @@
  * Licensed under the GPL, LGPL and MPL Open Source licenses.
 */
 	// Language
+$log = new B_LOG_(B_LOG_FILE);
+$log->write('defined(LANG)', defined('LANG'), 'LANG', LANG);
 	if(!defined('LANG')) define('LANG', 'ja');
