5925227648239b07079d7f17330a5b71fd00028e
BIGBEAT\nishida <nishida@bigbeat.co.jp>
2024-02-14 13:11:54 +0900

    fixed

--- bs-admin/module/contents/compare_tree.php
+++ bs-admin/module/contents/compare_tree.php
@@ -150,7 +150,7 @@
 			$this->compare($left, $right);
 			$root_node->compare($this->version_right['version_id'], $left, $right);
 
-			$open_node_path = isset($this->session['open_nodes_path']) ? $this->session['open_nodes_path'] : '';
+			$open_nodes_path = isset($this->session['open_nodes_path']) ? $this->session['open_nodes_path'] : '';
 			$list[] = $root_node->getNodeList($this->session['open_nodes'], $open_nodes_path, $node_id, $category);
 
 			$this->db->commit();
